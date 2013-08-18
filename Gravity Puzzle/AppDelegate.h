a7349a01700873a42589d8566d33bf127fee3f9e
jmason86 <jmason86@gmail.com>
2013-08-18 11:05:45 -0600

    Initial Commit

---
+++ Gravity Puzzle/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Gravity Puzzle
+//
+//  Created by James Mason on 8/18/13.
+//  Copyright (c) 2013 Trinary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
